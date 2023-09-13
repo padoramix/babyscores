93983a07e599d517b27afabe610dd9890c154adb
Gregory Devant <gdevant@banque-edel.fr>
2023-09-13 22:41:09 +0200

    ADD all user module logic, interface and api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express"
 import * as dotevnv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
+import { userRouter } from "./users/user.routes"
 
 dotevnv.config()
 
@@ -17,6 +18,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cors())
 app.use(helmet())
+
+// Setting up routes
+app.use('/', userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
