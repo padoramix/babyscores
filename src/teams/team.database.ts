2f2053a3afa0694fee669b3ae37d908ce4f09936
Gregory Devant <gdevant@banque-edel.fr>
2023-09-14 14:03:40 +0200

    UPD add team database comments

--- src/teams/team.database.ts
+++ src/teams/team.database.ts
@@ -2,8 +2,15 @@ import { Team, UnitTeam, Teams } from "./team.interface"
 import {v4 as random} from "uuid"
 import fs from "fs"
 
+// Array of teams
 let teams: Teams = loadTeams()
 
+/**
+ * loadTeams
+ * Load all teams from JSON DB
+ *
+ * @return {*}  {Teams}
+ */
 function loadTeams () : Teams {
   try {
     const data = fs.readFileSync("./src/db/teams.json", "utf-8")
@@ -14,6 +21,12 @@ function loadTeams () : Teams {
   }
 }
 
+/**
+ * saveTeams
+ * Save all teams inside JSON file
+ *
+ * @return void
+ */
 function saveTeams () {
   try {
     fs.writeFileSync("./src/db/teams.json", JSON.stringify(teams), "utf-8")
@@ -23,10 +36,30 @@ function saveTeams () {
   }
 }
 
+/**
+ * findAll
+ * Retrieves all the teams from JSON file
+ * 
+ * @return {*} {Teams}
+ */
 export const findAll = async (): Promise<UnitTeam[]> => Object.values(teams);
 
+/** 
+ * findOne
+ * Retrieves one team by ID
+ * 
+ * @param id: string
+ * @return {*} {Team}
+*/
 export const findOne = async (id: string): Promise<UnitTeam> => teams[id];
 
+/**
+ * findByName
+ * Retrieves one team by name
+ * 
+ * @param team_name: string
+ * @return {*} {Team} 
+ */
 export const findByName = async (team_name: string): Promise<null | UnitTeam> => {
   const allTeams = await findAll();
 
@@ -38,6 +71,14 @@ export const findByName = async (team_name: string): Promise<null | UnitTeam> =>
   return getTeam;
 }
 
+/**
+ * findByPlayers
+ * Retrieves one team by players
+ * 
+ * @param player_one_id: string
+ * @param player_two_id: string
+ * @return {*} {Team} 
+ */
 export const findByPlayers = async(player_one_id: string, player_two_id: string): Promise<null | UnitTeam> => {
   const allTeams = await findAll()
 
@@ -50,6 +91,13 @@ export const findByPlayers = async(player_one_id: string, player_two_id: string)
   return getTeam
 }
 
+/**
+ * create
+ * Create team
+ * 
+ * @param teamData: UnitTeam
+ * @return {*} {Team} 
+ */
 export const create = async (teamData: UnitTeam): Promise<UnitTeam | null> => {
 
   let id = random()
@@ -75,6 +123,14 @@ export const create = async (teamData: UnitTeam): Promise<UnitTeam | null> => {
   return team;
 }
 
+/**
+ * update
+ * Update a team (id) with updateValues(array)
+ * 
+ * @param id 
+ * @param updateValues 
+ * @returns {*} {Team}
+ */
 export const update = async (id: string, updateValues: Team) : Promise<UnitTeam | null> => {
   const teamExists = await findOne(id)
 
@@ -92,6 +148,13 @@ export const update = async (id: string, updateValues: Team) : Promise<UnitTeam 
   return teams[id]
 }
 
+/**
+ * remove
+ * Remove a team by ID
+ *
+ * @param id 
+ * @returns void
+ */
 export const remove = async (id: string) : Promise<null | void> => {
   const team = await findOne(id)
 
